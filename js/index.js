6e03be5c58d27bfd8edc4ab32eb5c9ef060f162b
samuellye98 <sl2982@cornell.edu>
2019-05-10 15:02:07 -0400

    updates

---
+++ js/index.js
@@ -0,0 +1,28 @@
+//requires carouFredsel.js
+//pen by Beatrize
+
+jQuery(document).ready(function() {
+  "use strict";
+  $(".carousel").carouFredSel({
+    responsive: true,
+    width: "100%",
+    circular: true,
+    scroll: {
+      item: 1,
+      duration: 500,
+      pauseOnHover: true
+    },
+    auto: true,
+    items: {
+      visible: {
+        min: 1,
+        max: 1
+      },
+      height: "variable"
+    },
+    pagination: {
+      container: ".sliderpagnation",
+      anchorBuilder: false
+    }
+  });
+});
